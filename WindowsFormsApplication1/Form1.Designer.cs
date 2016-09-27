4bda6a9fec917f25c063823cdaf9424354d96390
JackFendley <jackfendley18@gmail.com>
2016-09-27 10:29:24 -0500

    Finished product
    Assignment for Mr. Hardman's class.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.lblClick = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(100, 91);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Button";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblClick
+            // 
+            this.lblClick.AutoSize = true;
+            this.lblClick.Location = new System.Drawing.Point(97, 64);
+            this.lblClick.Name = "lblClick";
+            this.lblClick.Size = new System.Drawing.Size(64, 13);
+            this.lblClick.TabIndex = 1;
+            this.lblClick.Text = "Click Button";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblClick);
+            this.Controls.Add(this.btnClick);
+            this.Name = "Form1";
+            this.Text = "Hello User";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label lblClick;
+    }
+}
+
